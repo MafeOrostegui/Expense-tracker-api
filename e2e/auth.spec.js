20fe397ec530c9e7ae3775c42a409a77434aab6f
Mariaorostegui <mfop001new@gmail.com>
2023-12-13 18:32:37 -0500

    changes in the tests

--- e2e/auth.spec.js
+++ e2e/auth.spec.js
@@ -1,25 +1,25 @@
 const { fetch } = process;
 
 describe('POST /login', () => {
-  it('should respond with 400 when email and password missing', () => (
+  it('should resond with 400 when email and password missing', () => (
     fetch('/login', { method: 'POST' })
-      .then((resp) => expect(resp.status).toBe(400))
+      .then((res) => expect(res.status).toBe(400))
   ));
 
-  it('should respond with 400 when email is missing', () => (
+  it('should resond with 400 when email is missing', () => (
     fetch('/login', {
       method: 'POST',
       body: { email: '', password: 'xxxx' },
     })
-      .then((resp) => expect(resp.status).toBe(400))
+      .then((res) => expect(res.status).toBe(400))
   ));
 
-  it('should respond with 400 when password is missing', () => (
+  it('should resond with 400 when password is missing', () => (
     fetch('/login', {
       method: 'POST',
       body: { email: 'foo@bar.baz' },
     })
-      .then((resp) => expect(resp.status).toBe(400))
+      .then((res) => expect(res.status).toBe(400))
   ));
 
   it('fail with 404 credentials dont match', () => (
@@ -27,6 +27,6 @@ describe('POST /login', () => {
       method: 'POST',
       body: { email: `foo-${Date.now()}@bar.baz`, password: 'xxxx' },
     })
-      .then((resp) => expect(resp.status).toBe(400))
+      .then((res) => expect(res.status).toBe(400))
   ));
 });
