f1f3d57f846706a34f8a5d853d481dbd08404042
Mariaorostegui <mfop001new@gmail.com>
2023-12-13 18:18:08 -0500

    Update test Users

--- e2e/users.spec.js
+++ e2e/users.spec.js
@@ -10,20 +10,20 @@ describe('GET /users', () => {
 
 describe('GET /users/:uid', () => {
   it('should fail with 403 when no auth', () => (
-    fetch('/users/foo@bar.baz').then((resp) => expect(resp.status).toBe(403))
+    fetch('/users/example@gmail.com').then((resp) => expect(resp.status).toBe(403))
   ));
 });
 
 describe('PATCH /users/:uid', () => {
   it('should fail with 403 when no auth', () => (
-    fetch('/users/foo@bar.baz', { method: 'PATCH' })
+    fetch('/users/example@gmail.com', { method: 'PATCH' })
       .then((resp) => expect(resp.status).toBe(403))
   ));
 });
 
 describe('DELETE /users/:uid', () => {
   it('should fail with 403 when no auth', () => (
-    fetch('/users/foo@bar.baz', { method: 'DELETE' })
+    fetch('/users/example@gmail.com', { method: 'DELETE' })
       .then((resp) => expect(resp.status).toBe(403))
   ));
 });
